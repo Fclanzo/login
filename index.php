98bf6407ead8a68ee2fda061c129457a9fd4f345
Francesco C. Lanzoni <117127113+Fclanzo@users.noreply.github.com>
2022-11-18 22:22:29 -0300

    atualização em bootstrap do php

--- index.php
+++ index.php
@@ -5,18 +5,37 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 </head>
 <body>
+    <main class="container">
     <form action="validar.php" method="post">
-        Usuàrio:<input name="usuario"><br>
-        Senha:<input name="senha" type="password"><br>
-        <button type="submit">Entrar</button>
-        <button type="reset">Limpar</button>
+        <div class="row">
+            <div class="col">
+                <label class="form-label">Usuàrio:</label>
+                <input name="usuario" class="form-control">
+</div>
+<div class="col">
+<label class="form-label">Senha:</label>
+                <input name="senha" class="form-control">
+        
+</div>
+</div>
+<div class="row">
+    <div class="col text-center mt-3">
+        <button type="submit" class="btn btn-primary">Entrar</button>
+        <button type="reset" class="btn btn-secondary">Limpar</button>
+</div>
+</div>
 </form>
-<footer>
-    <p>Todos os direitos reservados &copy; <?php echo date("Y");?>
+</main>
+<footer class ="container-fluid bg-dark text-light text-center mt-5 pt-2 pb-1 fixed-bottom">
+<p>Todos os direitos reservados &copy; <?php echo date("Y");?>
 </p>
-<footer>
+</footer>
+
+
+
 <!--http://localhost/login/index.php -->
 </body>
 </html>
--- validar.php
+++ validar.php
@@ -2,5 +2,11 @@
 $usuario = $_POST['usuario'];
 $senha = $_POST['senha'];
 
-echo "$usuario - $senha";
+if($senha == 123 && $usuario == 'francesco'){
+    header('location: painel.php');
+}else{
+    header('location: usuario-invalido.php');
+    }
 ?>
+
+
